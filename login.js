3bbf3344d004c0bc7951a1403686adb99e7e9c86
msharma6138 <98579682+msharma6138@users.noreply.github.com>
2023-01-02 20:11:35 +0530

    Update login.js

--- login.js
+++ login.js
@@ -3,6 +3,16 @@ mport React from 'react'
 const Login=()=>{
 	return(
 	<div>
+		<form action="">
+		<div>
+		<label htmlFor="email"> EMAIL</label>
+		<input type="text" name="email" id="email"/>
+		<div/>
+		<div>
+		<label htmlFor="password"> PASSWORD</label>
+		<input type="text" name="passw" id="passw"/>
+		</div>
+		<button type="submit">SUBMIT</button>
 		<h1>Hello Form</h1>
 	</div>
 	)
